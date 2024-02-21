4a28cdb603f32f951931028e3043f79b8be0f55b
Developer_Sameer <105971029+sam12-4@users.noreply.github.com>
2024-02-22 04:04:39 +0500

    commit2 Day1

--- Day1.js
+++ Day1.js
@@ -4,3 +4,10 @@ const number = prompt("Enter your number");
 const result = Math.sqrt(number);
 console.log(`The square root of ${number} is ${result}`);
  
+// JavaScript Program to Calculate the Area of a Triangle
+
+let b = 30;
+let  h = 45;
+const area = b*h/2;
+console.log(`The area of triangle with base=${b}, height=${h} is ${area}`);
+
