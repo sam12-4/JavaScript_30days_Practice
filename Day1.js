2adfef051341e4e6accde5cf422e8b9caf73d7b9
Developer_Sameer <105971029+sam12-4@users.noreply.github.com>
2024-02-21 16:07:18 +0500

    Day1 commit 1

---
+++ Day1.js
@@ -0,0 +1,6 @@
+// JavaScript Program to Find the Square Root
+
+const number = prompt("Enter your number");
+const result = Math.sqrt(number);
+console.log(`The square root of ${number} is ${result}`);
+ 
