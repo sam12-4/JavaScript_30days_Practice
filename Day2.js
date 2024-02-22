ceccd72b4ac63747492d5e11187ffe23a6f7a1d1
Developer_Sameer <105971029+sam12-4@users.noreply.github.com>
2024-02-22 05:40:28 +0500

    Day2 commit1

---
+++ Day2.js
@@ -0,0 +1,37 @@
+// JavaScript Program to Swap Two Variables
+
+var v1 = 56;
+var v2 = 78;
+console.log("Before swapping:");
+console.log("Value of v1 is : "+v1);
+console.log("Value of v2 is :", v2);
+console.log("After swapping:");
+let temp = v1;
+var v1 = v2;
+var v2 = temp;
+console.log("Value of v1 after swapping is : ", v1);
+console.log("Value of v2 after swapping is : ", v2);
+
+// Javascript Program to Solve Quadratic Equation
+let root1, root2;
+let a = prompt("Enter the first number: ");
+let b = prompt("Enter the second number: ");
+let c = prompt("Enter the third number: ");
+
+let discriminant = Math.pow(b, 2) - 4*a*c;
+console.log(discriminant);
+
+if (discriminant  > 0){
+    root1 = (-b + Math.sqrt(discriminant))/ (2*a);
+    root2 = (-b - Math.sqrt(discriminant))/ (2*a);
+    console.log(`The roots are ${root1.toFixed(2)} and ${root2.toFixed(2)}`);
+}
+else if(discriminant == 0){
+    root1 = root2 = -b/(2*a);
+    console.log(`The roots are ${root1.toFixed(2)} and ${root2.toFixed(2)}`);
+}
+else{
+    let realPart = (-b /(2*a)).toFixed(2);
+    let imagpart = (Math.sqrt(-discriminant)/ (2*a)).toFixed(2);
+    console.log('The roots are '+realPart+' + i'+imagpart + ' and ' + realPart + " - i" + imagpart);
+}
